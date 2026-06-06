e19d6f73e98efbdffcd8769d61ef62b1931bb506
VSRDev09 <vinissantosz0009@gmail.com>
2026-06-06 08:37:09 -0300

    Log: show request method and URI in JwtAuthenticationFilter

--- backend/src/main/java/com/navcare/integration/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/navcare/integration/security/JwtAuthenticationFilter.java
@@ -28,6 +28,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
 
+        System.out.println("REQUEST => " + request.getMethod() + " " + request.getRequestURI()
+    );
         String token = extractBearerToken(request.getHeader(HttpHeaders.AUTHORIZATION));
         if (token == null) {
             filterChain.doFilter(request, response);
